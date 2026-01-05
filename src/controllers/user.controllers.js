b9796004a331c86279a18eda37399b3892d3bf0e
Temillionaire <olayinkatemitayo13@gmail.com>
2026-01-05 17:29:09 +0100

    Refactor error handling in signup to use console.log for debugging

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -39,8 +39,10 @@ await sendEmail(
 
         return res.status(201).json({ message: 'User registered successfully.' });
     } catch (error) {
-        console.error('Error during signup:', error);
-        return res.status(500).json({ message: 'Server Error' });
+        console.log(error);
+        
+        // console.error('Error during signup:', error);
+        // return res.status(500).json({ message: 'Server Error' });
     }};
 
 const login = async (req, res) => {
