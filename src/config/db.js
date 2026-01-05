0e49fecdb9c62e405a0c2e82fc6afc8f36989583
Temillionaire <olayinkatemitayo13@gmail.com>
2026-01-05 11:29:12 +0100

    first push

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/crud_auth_app';
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+        process.exit(1);
+    }  
+};
+
+module.exports = connectDB; 
---
+++ src/routes/user.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {register, login, forgetPassword, resetPassword, verifyotp, resendOtp, getAllUsers, } = require('../controllers/user.controllers');
+const isAuth = require('../config/auth');
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.put('/forget-password', forgetPassword);
+router.put('/reset-password', resetPassword);
+router.put('/verify-otp', verifyotp);
+router.put('/resend-otp', resendOtp);
+router.get('/get-All-users', isAuth, getAllUsers);
+
+module.exports = router;
